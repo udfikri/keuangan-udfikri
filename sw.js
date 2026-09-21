b40a0c19ce58f59930502570aacab77b2143b7f5
udfikri <udfikri01@gmail.com>
2026-09-21 19:42:05 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "ud-fikri-shell-v29";
+const CACHE_NAME = "ud-fikri-shell-v30";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -12,10 +12,10 @@ const APP_SHELL = [
   "./laporan.html",
   "./kelola-data.html",
   "./manifest.webmanifest",
-  "./css/style.css?v=29",
+  "./css/style.css?v=30",
   "./js/config.js?v=22",
   "./js/supabase.js?v=22",
-  "./js/app.js?v=29",
+  "./js/app.js?v=30",
   "./assets/logo-ud-fikri.png",
   "./assets/pwa-icon-180.png",
   "./assets/pwa-icon-192.png",
